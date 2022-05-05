7c4acd557d3bc23d8a1d74461e3ab334c943925a
DeadmanBob <github@traxel.com>
2022-05-05 06:10:37 -0700

    beginning to evolve soylent_green into the automated hacker history generator code

---
+++ python/lib/feature_selection.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+include_features = ['binFiles','textFiles','textLines','javascriptLines','rustLines',
+                    'lockLines','htmlLines','jsonLines','markdownLines','imgFiles']
+
+def make_feature_vector(feature_dicts, feature_names):
+    return [np.log1p(feature_dicts[name]) for name in feature_names]
+
+features_dict = {}
+X_full = []
+for commit_features in all_commit_features:
+    commit_id = commit_features['commit']
+    features_dict[commit_id] = commit_features
+    feature_vector = make_feature_vector(commit_features, include_features)
+    X_full.append(feature_vector)
+
+X = []
+y = []
+for commit_id, labelling_spreadsheet_row in data_df.iterrows():
+    commit_features = features_dict[commit_id]
+    feature_vector = make_feature_vector(commit_features, include_features)
+    target_label = labelling_spreadsheet_row[0]
+    X.append(feature_vector)
+    y.append(target_label)
