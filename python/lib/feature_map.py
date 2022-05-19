8eb7375b7c163f6e1dc96dacbe00b7af8372591e
DeadmanBob <github@traxel.com>
2022-05-19 13:07:46 -0700

    removed stale hard coded action types

--- python/lib/feature_map.py
+++ python/lib/feature_map.py
@@ -1,33 +1,9 @@
 import os
 import json
 
-def make_action( encoding, action, file_types ):
-    return dict( encoding=encoding, action=action, file_types=file_types )
-
 def get_actions(project_root_path):
     in_path = project_root_path + '/python/cfg/action_types.json'
     return json.loads(open(in_path, 'r').read())
-    # return [
-    #     make_action( 'binary', 'img', ['png', 'jpg', 'gif', 'drawio', 'svg'] ),
-    #     make_action( 'binary', 'noextbin', ['noextbin'] ),
-    #     make_action( 'text', 'clojure', ['clj']),
-    #     make_action( 'text', 'configuration', ['ini', 'cfg', 'conf']),
-    #     make_action( 'text', 'datascience', ['ipynb']),
-    #     make_action( 'text', 'gitignore', ['gitignore']),
-    #     make_action( 'text', 'html', ['html', 'css', 'scss']),
-    #     make_action( 'text', 'javascript', ['js', 'jsx', 'ts', 'tsx', 'vue'] ),
-    #     make_action( 'text', 'json', ['json'] ),
-    #     make_action( 'text', 'lock', ['lock'] ),
-    #     make_action( 'text', 'markdown', ['md'] ),
-    #     make_action( 'text', 'noexttext', ['noexttext']),
-    #     make_action( 'text', 'pydata', ['rst'] ), # ReStructuredText, python data
-    #     make_action( 'text', 'python', ['py'] ),
-    #     make_action( 'text', 'rust', ['rs', 'toml'] ),
-    #     make_action( 'text', 'shell', ['sh']),
-    #     make_action( 'text', 'solidity', ['sol']),
-    #     make_action( 'text', 'txt', ['txt']),
-    #     make_action( 'text', 'yarn', ['yml', 'yaml']),
-    # ]
 
 def percent(numerator, denominator):
     if (numerator == 0): return 0
@@ -50,7 +26,7 @@ def addTextFeatures(typeArray, statDict, statName, file_types):
     statDict[statName + 'FilePct'] = percent(statDict[statName + 'Files'], statDict['totalFiles'])
     statDict[statName + 'LinePct'] = percent(statDict[statName + 'Lines'], statDict['textLines'])
 
-def extract_features(project_root_path, commit):
+def extract_features(actions, commit):
     typeDict = commit['fileTypes']
     typeArray = [{'fileType': key, 'stats': typeDict[key]} for key in typeDict.keys()]
     textTypeArray = [typeEntry for typeEntry in typeArray if typeEntry['stats']['inserts'] > 0 or typeEntry['stats']['deletes'] > 0]
@@ -68,7 +44,6 @@ def extract_features(project_root_path, commit):
     statDict['textFiles'] = sum_stat(textTypeArray, 'occurrences')
     statDict['textLines'] = sum_stat(textTypeArray, 'inserts')
 
-    actions = get_actions(project_root_path)
     for action in actions:
         action_code = action['action']
         types = action['file_types']
@@ -80,9 +55,10 @@ def extract_features(project_root_path, commit):
     return statDict
 
 def get_commit_features(project_root_path, all_commit_stats):
+    actions = get_actions(project_root_path)
     all_features = []
     for commit in all_commit_stats:
         commit_id = commit['commit']
-        commit_features = extract_features(project_root_path, commit)
+        commit_features = extract_features(actions, commit)
         all_features.append(commit_features)
     return all_features;
