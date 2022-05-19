eeb9ec7d1bf45651c0c9b6efa4bab881c473cb7d
DeadmanBob <github@traxel.com>
2022-05-19 13:08:31 -0700

    added datetime conversion

--- python/lib/commit_stats.py
+++ python/lib/commit_stats.py
@@ -1,5 +1,7 @@
 import os
 import json
+import hashlib
+from datetime import datetime
 
 def find_files(name, path):
     result = []
@@ -8,16 +10,36 @@ def find_files(name, path):
             result.append(os.path.join(root, name))
     return result
 
-def parse_commits(commit_files):
+def parse_commits(commit_files, stat_file_name):
     all_logs = []
     all_commits = []
     for file in commit_files:
-        in_stats = json.loads(open(file, 'r').read());
+        path_pos = file.find(stat_file_name)
+        path_parts = file[:path_pos].split('/')
+        first_part = path_parts[len(path_parts) - 3]
+        second_part = path_parts[len(path_parts) - 2]
+        project = first_part + '/' + second_part
+        in_stats = json.loads(open(file, 'r').read())
         all_logs.append(in_stats)
         for commit in in_stats:
+            commit['seen_in'] = [project]
+            
+            author = commit['Author']
+            author_hash = hashlib.md5(bytes(author, 'utf-8')).hexdigest()
+            commit['author_hash'] = author_hash
+            
+            dtstring = commit['Date']
+            # dtform = 'Date:    %a %b %d %H:%M:%S %Y %z'
+            # dt = datetime.strptime(dtstring, dtform)
+            dt = datetime.fromisoformat(dtstring)
+            iso_datetime = dt.isoformat()
+            utc_datetime = datetime.utcfromtimestamp(dt.timestamp()).isoformat()
+            commit['iso_datetime'] = iso_datetime
+            commit['utc_datetime'] = utc_datetime
+            
             all_commits.append(commit)
     return all_commits
 
-def load_commit_stats(name, path):
-    files = find_files(name, path)
-    return parse_commits(files)
+def load_commit_stats(stat_file_name, path):
+    files = find_files(stat_file_name, path)
+    return parse_commits(files, stat_file_name)
