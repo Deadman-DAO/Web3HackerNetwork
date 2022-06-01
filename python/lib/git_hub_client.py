64596943ef2446338015530c4183f4fe872cd62b
enigmatt <git@theenkes.com>
2022-05-31 18:13:12 -0700

    I was so happy with the newest model of modularized autonomous threads now making three different API calls to GitHub in order to acquire more info about a repos. And then... "Process finished with exit code -1073741819 (0xC0000005)" That's apparently a Winnows Access Control violation, hopefully set off by the debugger.

---
+++ python/lib/git_hub_client.py
@@ -0,0 +1,78 @@
+import os
+import sys
+from socket import gethostname
+import requests
+import time
+
+
+def fetch_json_value(key, json):
+    cur_json = json
+    split = key.split('.')
+    if len(split) > 1:
+        for k in split:
+            cur_json = fetch_json_value(k, cur_json)
+        return cur_json
+    else:
+        if key in json:
+            return json[key]
+        raise StopIteration(''.join(('key ', key, ' not found.')))
+
+
+class GitHubClient:
+    def __init__(self, git_hub_lock):
+        self.git_hub_lock = git_hub_lock
+        self.machine_name = os.uname().nodename if sys.platform != "win32" else gethostname()
+        self.error_count = 0
+        self.overload_count = 0
+        self.good_reply_code_count = 0
+        self.MAX_LOOPS = 6
+        self.incomplete_count = 0
+        with open('./web3.github.token', 'r') as f:
+            self.token = f.readline()
+            self.token = self.token.strip('\n')
+            self.headers = {'Authorization': 'token %s' % self.token}
+
+    def fetch_with_lock(self, url):
+        try:
+            self.git_hub_lock.acquire()
+            time.sleep(1)
+            return requests.get(url, headers=self.headers)
+        except Exception as e:
+            print('Error encountered calling', url, e)
+            return None
+        finally:
+            self.git_hub_lock.release()
+
+    def fetch_json_with_lock(self, url, recurse_count=0):
+        json_ret_val = None
+        reply = self.fetch_with_lock(url)
+
+        if reply is None:
+            self.error_count += 1
+            print('No response received from API call to GitHub', url)
+        elif reply.status_code == 403:
+            self.overload_count += 1
+            # We've exceed our 5000 calls per hour!
+            print('Maximum calls/hour exceeded! Sleeping', recurse_count, 'minute(s)')
+            print('Working on', url)
+            time.sleep(60*recurse_count)
+            if recurse_count <= self.MAX_LOOPS:
+                json_ret_val = self.fetch_json_with_lock(url, recurse_count+1)
+        elif reply.status_code == 202:
+            self.incomplete_count += 1
+            print('GitHub is "still working on"', url)
+            time.sleep(30*recurse_count)
+            if recurse_count <= self.MAX_LOOPS:
+                json_ret_val = self.fetch_json_with_lock(url, recurse_count+1)
+        elif reply.status_code == 200:
+            try:
+                self.good_reply_code_count += 1
+                json_ret_val = reply.json()
+            except requests.exceptions.JSONDecodeError as e:
+                print('Error encountered parsing reply from',url,e)
+        else:
+            self.error_count += 1
+            print('ERROR - Status code:', reply.status_code, 'encountered ', url)
+
+        return json_ret_val
+
---
+++ python/lib/repository_investigator.py
@@ -0,0 +1,146 @@
+import multiprocessing
+import time
+from db_dependent_class import DBDependent
+from monitor import MultiprocessMonitor, timeit
+from git_hub_client import GitHubClient
+from git_hub_client import fetch_json_value
+from datetime import datetime as datingdays
+
+class Contributor:
+    def __init__(self, login):
+        self.login = login
+        self.change_count = 0
+        self.start_date = datingdays.now().timestamp()
+        self.end_date = 0
+    def add_week(self, week_start_timestamp, count):
+        self.change_count += count
+        if self.start_date > week_start_timestamp:
+            self.start_date = week_start_timestamp
+        if self.end_date < week_start_timestamp:
+            self.end_date = week_start_timestamp
+
+class Investigator(DBDependent, GitHubClient):
+    def __init__(self, lock):
+        GitHubClient.__init__(self, lock)
+        DBDependent.__init__(self)
+        self.repo_last_year = None
+        self.url_prefix = 'https://api.github.com/repos/'
+        self.url_contributors = '/stats/contributors'
+        self.url_activity = '/stats/commit_activity'
+        self.lock = lock
+        self.repo_owner = ''
+        self.repo_name = ''
+        self.created_at = None
+        self.updated_at = None
+        self.pushed_at = None
+        self.homepage = None
+        self.size = None
+        self.watchers_count = None
+        self.forks_count = None
+        self.network_count = None
+        self.subscribers_count = None
+        self.contributors = {}
+        self.repo_contributor = None
+
+    def form_repo_url(self):
+        return ''.join((self.url_prefix, self.repo_owner, '/', self.repo_name))
+
+    def form_contributors_url(self):
+        return ''.join((self.form_repo_url(), self.url_contributors))
+
+    def form_activity_url(self):
+        return ''.join((self.form_repo_url(), self.url_activity))
+
+    @timeit
+    def reserve_new_repo(self):
+        success = False
+        self.get_cursor().callproc('ReserveNextRepoForEvaluation', [self.machine_name])
+        result = self.cursor.fetchone()
+        if result is not None:
+            self.repo_owner = result[0]
+            self.repo_name = result[1]
+            success = True
+        return success
+
+
+    @timeit
+    def fetch_repo_info(self):  # Raises StopIteration Exception
+        json = self.fetch_json_with_lock(self.form_repo_url())
+        if json is None:
+            raise StopIteration('Restful Response did not form a parseable JSON document', self.form_repo_url())
+
+        self.created_at = fetch_json_value('created_at', json)
+        self.updated_at = fetch_json_value('updated_at', json)
+        self.pushed_at = fetch_json_value('pushed_at', json)
+        self.homepage = fetch_json_value('homepage', json)
+        self.size = fetch_json_value('size', json)
+        self.watchers_count = fetch_json_value('watchers_count', json)
+        self.forks_count = fetch_json_value('forks_count', json)
+        self.network_count = fetch_json_value('network_count', json)
+        self.subscribers_count = fetch_json_value('subscribers_count', json)
+
+
+    @timeit
+    def fetch_contributor_info(self):
+        json = self.fetch_json_with_lock(self.form_contributors_url())
+        if json is None:
+            raise StopIteration('Restful Response did not form a parseable JSON document', self.form_contributors_url())
+        self.contributors = []
+        self.repo_contributor = Contributor('contributor_counts')
+        for contributor in json:
+            # JSON doc is an array of "contributor" objects
+            total = fetch_json_value('total', contributor)
+            author = fetch_json_value('author.login', contributor)
+            c = Contributor(author)
+            self.contributors.append(c)
+            weeks = fetch_json_value('weeks', contributor)
+            for w in weeks:
+                ts = fetch_json_value('w', w)
+                added = fetch_json_value('a', w)
+                deleted = fetch_json_value('d', w)
+                changed = fetch_json_value('c', w)
+                ttl = added+deleted+changed;
+                if ttl > 0:
+                    c.add_week(ts, ttl)
+                    self.repo_contributor.add_week(ts, ttl)
+
+    @timeit
+    def fetch_activity_info(self):
+        json = self.fetch_json_with_lock(self.form_activity_url())
+        if json is None:
+            raise StopIteration('Restful Response did not form a parseable JSON document', self.form_contributors_url())
+        self.repo_last_year = Contributor('last_year_activity')
+        for week in json:
+            total = fetch_json_value('total', week)
+            ts = fetch_json_value('week', week)
+            if total > 0:
+                self.repo_last_year.add_week(ts, total)
+
+    @timeit
+    def write_results_to_database(self):
+        print('Nearly there')
+
+    @timeit
+    def sleep_it_off(self):
+        time.sleep(60)
+
+    def main(self):
+        running = True
+        while running:
+            if self.reserve_new_repo():
+                try:
+                    self.fetch_repo_info()
+                    self.fetch_contributor_info()
+                    self.fetch_activity_info()
+                    self.write_results_to_database()
+                except StopIteration as si:
+                    print(si)
+            else:
+                self.sleep_it_off()
+
+
+if __name__ == "__main__":
+    Investigator(multiprocessing.RLock()).main()
+else:
+    print(__name__)
+
