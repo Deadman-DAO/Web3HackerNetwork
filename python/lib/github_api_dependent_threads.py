a7bc7b20e3589be1896e24c4314bc1e0496a135e
enigmatt <git@theenkes.com>
2022-08-03 12:56:34 -0700

    Cleaned up the cleanup process. :)

--- python/lib/github_api_dependent_threads.py
+++ python/lib/github_api_dependent_threads.py
@@ -1,5 +1,6 @@
 from GitHubUserIDFinder import GitHubUserIDFinder
 from MultiprocessManager import MultiprocessManager
+from python.lib.post_mortem_cleanup import PostMortemCleanerUpper
 from repository_investigator import Investigator
 from trace_author_commit_history import AuthorCommitHistoryProcessor
 
@@ -8,7 +9,8 @@ class GithubThreadMgr(MultiprocessManager):
     def get_process_list(self):
         return {'achp ': AuthorCommitHistoryProcessor,
                 'ghuif': GitHubUserIDFinder,
-                'inv  ': Investigator}
+                'inv  ': Investigator,
+                'clean': PostMortemCleanerUpper}
 
 
 if __name__ == "__main__":
