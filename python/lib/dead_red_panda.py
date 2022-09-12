fe222a18fcacfec56994740225aa31881346bbe7
enigmatt <git@theenkes.com>
2022-09-12 14:28:08 -0700

    Okay.  I've got a good example of a simple data structure.  Now to see if Panda/SQLAlchemy can work with a more complex dictionary.  Saving simple example before breaking it down.

--- python/lib/dead_red_panda.py
+++ python/lib/dead_red_panda.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from sqlalchemy import create_engine
 from db_dependent_class import DBDependent
-import mysql.connector
+from datetime import datetime as dt
 
 
 class RedDeadPandademption(DBDependent):
@@ -13,10 +13,18 @@ class RedDeadPandademption(DBDependent):
             self.cfg = json.load(r)
 
     def main(self):
+        self.get_cursor()
+        start_time = dt.now().timestamp()
         data_frame = []
-        self.get_cursor().execute("select md5, name_email from hacker_update_queue")
+        self.get_cursor().execute("select md5, name_email, id from hacker_update_queue")
+        cnt = 0
         for row in self.get_cursor():
-            data_frame.append(row)
+            cnt += 1
+            print(row[2])
+            data_frame.append([row[0], row[1]])
+        elapsed = dt.now().timestamp() - start_time
+        print(cnt, 'records loaded in ', elapsed)
+
         df = pd.DataFrame(data=data_frame, columns=['md5', 'name_email'])
         engine = create_engine("mariadb://{user}:{pw}@{host}/{db}".format(
             host=self.cfg['host'],
@@ -24,6 +32,8 @@ class RedDeadPandademption(DBDependent):
             user=self.cfg['user'],
             pw=self.cfg['password']))
         df.to_sql('hacker_update_queue', engine, index=False, if_exists='append')
+        elapsed = dt.now().timestamp() - start_time
+        print('Total execution time:', elapsed)
 
 
 if __name__ == '__main__':
