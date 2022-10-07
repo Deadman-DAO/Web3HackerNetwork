28158fe72c1823fdbfb781ed04c1da8460e0e37b
enigmatt <git@theenkes.com>
2022-10-07 14:25:36 -0700

    Adding stack trace generation

--- python/lib/minimum_dependency_processor.py
+++ python/lib/minimum_dependency_processor.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 def execute_analysis(owner=None, repo_name=None, numstat=None, repo_path=None):
-    sleepy_time = random.Random().random() * 10
+    sleepy_time = random.Random().random()
     print('#TST Executing analysis for {0}/{1} sleeping {2}'.format(owner, repo_name, sleepy_time))
     sleep(sleepy_time)
     print('#TST Finished analysis for {0}/{1}'.format(owner, repo_name))
--- python/lib/scalable_repo_tasks.py
+++ python/lib/scalable_repo_tasks.py
@@ -3,11 +3,14 @@ from repo_analyzer import RepoAnalyzer
 from repo_cleaner import RepoCleanup
 from repo_cloner import RepoCloner;
 from repo_numstat_gatherer import RepoNumstatGatherer
+import faulthandler
+import signal
 
 
 class ScalableRepoTasks(MultiprocessManager):
     def __init__(self):
         MultiprocessManager.__init__(self)
+        faulthandler.register(signal.SIGUSR1)
 
     def get_process_list(self):
         return {'clone': RepoCloner,
