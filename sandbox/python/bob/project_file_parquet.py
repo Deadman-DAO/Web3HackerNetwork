62860e1fc4dda98226ddcddcf3f8aae33e1047ca
DeadmanBob <github@traxel.com>
2022-10-09 16:57:56 -0700

    latest version of the parquet demo code

--- sandbox/python/bob/project_file_parquet.py
+++ sandbox/python/bob/project_file_parquet.py
@@ -1,13 +1,17 @@
-import bz2
-import json
+import configparser
 import datetime
 import dateutil.parser
+import duckdb
 import hashlib
 import numpy as np
 import os
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pyarrow.fs as pafs
+import sys
+
+sys.path.append("../matt/")
+from repo_grabber import RepoGrabber
 
 home_dir = os.path.expanduser("~")
 aws_credentials_path = home_dir+"/.aws/credentials"
@@ -17,14 +21,12 @@ repo_file_path = raw_path+"/repo_file"
 print(datetime.datetime.now())
 owner = 'apache'
 repo_name = 'ant'
-repo_path = '../../data/bob/numstat/'+owner+'/'+repo_name
-numstat_path = repo_path+'/log_numstat.out.json.bz2'
-numstat_raw = ''
-with open(numstat_path, 'rb') as r:
-    numstat_raw = r.read()
+#repo_name = 'kafka'
+owner = 'cilium'
+repo_name = 'cilium'
 
-numstat_str = bz2.decompress(numstat_raw)
-numstat_object = json.loads(numstat_str)
+numstat_object = RepoGrabber().get_numstat(owner, repo_name)
+
 print(datetime.datetime.now())
 
 def synthetic_partition_key(owner, repo_name):
@@ -99,61 +101,106 @@ def create_table(repo_files):
     col_total_deletes = pa.array(total_deletes_array)
     col_binary = pa.array(binary_array)
 
-    data = [col_partition_key, col_owner, col_repo_name, col_file_path,
+    data = [col_owner, col_repo_name, col_file_path,
             col_num_commits, col_first_commit_date, col_last_commit_date,
-            col_total_inserts, col_total_deletes, col_binary]
-    column_names = ["partition_key", "owner", "repo_name", "file_path",
+            col_total_inserts, col_total_deletes, col_binary,
+            col_partition_key]
+    column_names = ["owner", "repo_name", "file_path",
                     "num_commits", "first_commit_date", "last_commit_date",
-                    "total_inserts", "total_deletes", "binary"]
+                    "total_inserts", "total_deletes", "binary",
+                    "partition_key"]
     batch = pa.RecordBatch.from_arrays(data, column_names)
     inferred_table = pa.Table.from_batches([batch])
     explicit_fields = [
-        pa.field("partition_key", pa.string()),
         pa.field("owner", pa.string()),
         pa.field("repo_name", pa.string()),
         pa.field("file_path", pa.string()),
         pa.field("num_commits", pa.int64()),
-        pa.field("first_commit_date", pa.timestamp('s', tz='+00:00')),
-        pa.field("last_commit_date", pa.timestamp('s', tz='+00:00')),
+        pa.field("first_commit_date", pa.timestamp('us', tz='UTC')),#+00:00')),
+        pa.field("last_commit_date", pa.timestamp('us', tz='UTC')),#+00:00')),
         pa.field("total_inserts", pa.int64()),
         pa.field("total_deletes", pa.int64()),
-        pa.field("binary", pa.int8())
+        pa.field("binary", pa.int8()),
+        pa.field("partition_key", pa.string()),
     ]
     explicit_schema = pa.schema(explicit_fields)
     explicit_table = inferred_table.cast(explicit_schema)
     return explicit_table
 
-def update_parquet(owner, repo_name, table):
+def get_s3fs():
+    credentials = configparser.ConfigParser()
+    credentials.read(filenames=[aws_credentials_path])
+    enigmatt_access_key = credentials.get("enigmatt", "aws_access_key_id")
+    enigmatt_secret_key = credentials.get("enigmatt", "aws_secret_access_key")
     s3fs = pafs.S3FileSystem(access_key=enigmatt_access_key,
                              secret_key=enigmatt_secret_key)
+    return s3fs
+
+def read_partition_dataset(owner, repo_name, s3fs):
     partition_key = synthetic_partition_key(owner, repo_name)
-    partition_path = repo_file_path+"/partition_key="+partition_key+"/"
-    # s3fs = fs.FileSystem.from_uri("s3://")[0]
-    # load existing dataset
+    partition_path = repo_file_path
     dataset = pq.ParquetDataset(partition_path,
                                 filesystem=s3fs,
                                 partitioning="hive")
+    return dataset
+
+def update_parquet(owner, repo_name, table):
+    s3fs = get_s3fs()
+    duck_conn = duckdb.connect()
+    partition_key = synthetic_partition_key(owner, repo_name)
+    partition_path = repo_file_path
+    # s3fs = fs.FileSystem.from_uri("s3://")[0]
+    # load existing dataset
+    legacy_dataset = read_partition_dataset(owner, repo_name, s3fs)
+    legacy_table = legacy_dataset.read()#to_table()
+    #print(str(legacy_table.to_pandas()))
+    
     # select from existing where (owner != owner or repo_name != repo_name)
+    sql = f"""SELECT *
+                FROM legacy_table
+                WHERE partition_key = '{partition_key}'
+                  AND (
+                    owner != '{owner}'
+                    OR
+                    repo_name != '{repo_name}'
+                  )"""
+    other_repos_table = duck_conn.execute(sql).arrow()
+    print("other repos dataset:")
+    print(str(other_repos_table.to_pandas()))
+    print("this repo dataset:")
+    print(str(table.to_pandas()))
+    # die die die my darling
     # append to table
+    merged_table = pa.concat_tables([other_repos_table, table], promote=False)
+    merged_table = merged_table.sort_by([('owner','ascending'),
+                                         ('repo_name','ascending'),
+                                         ('file_path','ascending')])
     # delete existing partition from S3
     s3fs.delete_dir(partition_path)
     # write merged partition dataset to S3
-    pq.write_to_dataset(table,
+    pq.write_to_dataset(merged_table,
                         root_path='numstat-bucket/data_pipeline/raw/repo_file',
                         partition_cols=['partition_key'],
                         filesystem=s3fs)
 
-def update_repo_files_parquet(owner, repo_name, numstat_object, repo_path):
+def update_repo_files_parquet(owner, repo_name, numstat_object, repo_path="ignored"):
     repo_files = extract_repo_file_data(owner, repo_name, numstat_object)
     table = create_table(repo_files)
+    #print(str(table.to_pandas()))
     update_parquet(owner, repo_name, table)
-    
 
-update_repo_files_parquet(owner, repo_name, numstat_object, repo_path)
+
+update_repo_files_parquet(owner, repo_name, numstat_object)
 print(datetime.datetime.now())
 
-table = pq.read_table("repo_file")
-print(table.to_pandas())
+s3fs = get_s3fs()
+duck_conn = duckdb.connect()
+legacy_dataset = read_partition_dataset(owner, repo_name, s3fs)
+legacy_table = legacy_dataset.read()#to_table()
+
+print()
+print("What Was Written:")
+print(legacy_table.to_pandas())
 
 # to delete the file, I had to use the full path:
 # $ aws s3 rm s3://numstat-bucket/data_pipeline/raw/repo_file/partition_key=e3/10330317b031417c8ef039acff04b420-0.parquet
