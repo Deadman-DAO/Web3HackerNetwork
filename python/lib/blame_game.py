d4cefdc7bf69ce794dd1aa278829055a35a55d3d
enigmatt <git@theenkes.com>
2022-10-14 12:46:54 -0700

    Throwing in blame game example of regex processing

--- python/lib/blame_game.py
+++ python/lib/blame_game.py
@@ -1,7 +1,10 @@
 from monitor import MultiprocessMonitor, timeit
-from signal_handler import SignalHandler
+from signal_handler \
+    import SignalHandler
 import os
 import sys
+import re
+from re import findall as bitty, split as squat
 
 
 class BlameGameRetriever(SignalHandler):
@@ -15,8 +18,9 @@ class BlameGameRetriever(SignalHandler):
         self.max_wait = 60
 
     @timeit
-    def get_blame_game(self, filename) -> dict:
+    def get_blame_game(self, filename, compiled_regex_pattern=None) -> (dict, list):
         hacker_contribution_map = {}
+        dependency_list = []
         full_name = os.path.join(self.repo_dir, filename)
         if not os.path.exists(full_name):
             return None
@@ -36,12 +40,22 @@ class BlameGameRetriever(SignalHandler):
                         line_count += 1
                         who_when_line = line[42:].split(')')[0]
                         items = who_when_line.split(' ')
+
+                        if compiled_regex_pattern:
+                            source = line[len(line.split(')')[0])+2:]
+                            for n in compiled_regex_pattern.findall(source):
+                                if n not in dependency_list:
+                                    if isinstance(n, tuple):
+                                        dependency_list.append('.'.join(n) if n[0] else n[1])
+                                    else:
+                                        dependency_list.append(n)
+
                         if len(items) > 4:
                             line = int(items[len(items) - 1])
-                            if words[0] in self.commit_author_map:
+                            if words[1] in self.commit_author_map:
                                 # commit hash found in commit_author_map
                                 # retrieving author's md5sum
-                                author_hash = self.commit_author_map[words[0]]
+                                author_hash = self.commit_author_map[words[1]]
                                 if author_hash not in hacker_contribution_map:
                                     hacker_contribution_map[author_hash] = 0
                                 hacker_contribution_map[author_hash] += 1
@@ -49,4 +63,27 @@ class BlameGameRetriever(SignalHandler):
                 print('Error processing blame output:', e)
             for key in hacker_contribution_map:
                 hacker_contribution_map[key] = hacker_contribution_map[key] / (line_count if line_count > 0 else 1.0)
-            return hacker_contribution_map
+            return hacker_contribution_map, dependency_list
+
+
+if __name__ == "__main__":
+    ### Testing ###
+    map = {}
+    dir = '../'
+    me = BlameGameRetriever(dir, map)
+    reggy = re.compile("(?m)^(?:from[ ]+(\w+)[ ]+)?import[ ]+(\w+)(?:[ ]+as[ ]+\w+)?[ ]*$")
+    dic, jane = me.get_blame_game('./python/lib/file_hacker_parquet.py', reggy)
+    print(str(dic))
+    print(str(jane))
+    reggy = re.compile("(?m)^(?:from[ ]+(\w+)[ ]+)?import[ ]+(\w+)(?:[ ]+as[ ]+\w+)?[ ]*$")
+    dic, jane = me.get_blame_game('./python/lib/blame_game.py', reggy)
+    print(str(dic))
+    print(str(jane))
+    reggy = re.compile("^use ([^:^;]*)[;:].*$")
+    dic, jane = me.get_blame_game('./sandbox/python/bob/main.rs', reggy)
+    print(str(dic))
+    print(str(jane))
+    reggy = re.compile("^import ([^;]*);$")
+    dic, jane = me.get_blame_game('./sandbox/python/bob/SpotifyApi.java', reggy)
+    print(str(dic))
+    print(str(jane))
