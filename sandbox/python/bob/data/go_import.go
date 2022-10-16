ccfc5b036e77f42c11d852fe9de66b6fe8f3c50e
DeadmanBob <github@traxel.com>
2022-10-16 12:34:02 -0700

    go logic appears to be working

--- sandbox/python/bob/data/go_import.go
+++ sandbox/python/bob/data/go_import.go
@@ -6,15 +6,15 @@ import "two_of_seven"        // import packages one by one
 import foobarbaz "three_of_seven"
 
 import (
-_  "four_of_seven"
+_  "four_of_seven" // nasty "quoted string" in a comment
         
         "five_of_seven"
 )
 
 
 import
-   (
-
+ (
+        // some comments have a closing paren: )
 	"six/of/seven"
 )
 
--- sandbox/python/bob/regex/go_import.py
+++ sandbox/python/bob/regex/go_import.py
@@ -28,22 +28,94 @@ if match:
     print(match.groups())
 
 print("---------- FULL SOURCE FINDALL ----------------")
-pattern = r'multiline(?:.)*(_potato_)(?:.)*multiline'
-print(re.findall(pattern, full_source, re.MULTILINE))
-print(re.findall(pattern, full_source, re.DOTALL))
-print(re.findall(pattern, full_source, re.MULTILINE & re.DOTALL))
+# pattern = r'multiline(?:.)*(_potato_)(?:.)*multiline'
+# print(re.findall(pattern, full_source, re.MULTILINE))
+# print(re.findall(pattern, full_source, re.DOTALL))
+# print(re.findall(pattern, full_source, re.MULTILINE & re.DOTALL))
 
-print("---------- READING LINE BY LINE ---------------")
-for go in go_source:
-    print(f'testing:{go}')
-    match = go_import_regex.search(full_source)
-    if match:
-        print(match.groups())
+depends = list()
 
-print("---------- COPY OF EXAMPLE CODE ---------------")
-mystring="""This is some random text.
-Hello World.
-This is Goodbye.
+# capture single-line imports
+full_source = ""
+go_source = open('../data/go_import.go', 'r')
+for go_line in go_source:
+    print(go_line)
+    go_line = re.sub("//.*", "", go_line)
+    print(go_line)
+    deps = re.findall('import[^\"]*\"([^\"]*)\"', go_line)
+    depends.extend(deps)
+    full_source += go_line
+
+print(full_source)
+# capture multi-line imports
+# go_source = open('../data/go_import.go', 'r')
+# full_source = go_source.read()
+ptrn = "import[^\(]*\(([^\)]*)\)"
+print(re.findall(ptrn, full_source, re.DOTALL))
+lines = re.findall(ptrn, full_source, re.DOTALL)
+for line in lines:
+    deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
+    depends.extend(deps)
+
+print(list(set(depends)))
+
+print("--------- 2 match dotall ---------------")
+mystring = """import ("foo")
+import (
+"bar"
+)
+import ( "zoom" // annoying comment about "dave"
+"schwartz"
+)
 """
+ptrn = "import \(([^\)]*)\)"
+print(re.findall(ptrn, mystring, re.DOTALL))
+lines = re.findall(ptrn, mystring, re.DOTALL)
+depends = list()
+for line in lines:
+    deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
+    depends.extend(deps)
+print(depends)
 
-print(re.findall("^This.*", mystring, re.MULTILINE))
+
+def borken():
+    print("------------- 2 match dotal 2 ------------")
+    ptrn = "import \([^\"\)]*\"([^\"]*)\"[^\"\)]*\)"
+    print(re.findall(ptrn, mystring, re.DOTALL))
+
+    print("------------- 2 match dotal 2 ------------")
+    ptrn = "import +\((?:[^\"\)]*\"([^\"]*)\"[^\"\)]*)*\)"
+    print(re.findall(ptrn, mystring, re.DOTALL))
+
+    print("---------- EXAMPLE MULTILINE ---------------")
+    mystring="""This is some random text.
+    Hello World.
+    This is Goodbye.
+    """
+
+    print(re.findall("^This.*", mystring, re.MULTILINE))
+
+
+    print("---------- EXAMPLE DOTALL ---------------")
+    mystring="""This is some random text.
+    Hello World.
+    This is Goodbye.
+    """
+
+    print(re.findall("^This.*", mystring, re.DOTALL))
+
+    print("---------- EXAMPLE DOTALL & MULTILINE ---------------")
+    mystring="""This is some random text.
+    Hello World.
+    This is Goodbye.
+    """
+
+    print(re.findall("^This.*", mystring, re.DOTALL & re.MULTILINE))
+
+    print("---------- READING LINE BY LINE ---------------")
+    for go in go_source:
+        print(f'testing:{go}')
+        match = go_import_regex.search(full_source)
+        if match:
+            print(match.groups())
+
