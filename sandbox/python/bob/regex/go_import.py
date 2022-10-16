17bf28d5dceeed6d78c6b1f06e39a424c82f0a5e
DeadmanBob <github@traxel.com>
2022-10-16 12:52:13 -0700

    working go dependency regex, before removing extraneous sandbox code

--- sandbox/python/bob/regex/go_import.py
+++ sandbox/python/bob/regex/go_import.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 '''
 Breadcrumbs:
@@ -12,70 +13,87 @@ https://coderslegacy.com/python/regex-match-multiline-text/
 https://www.thegeekstuff.com/2014/07/advanced-python-regex/
 '''
 
-go_source = open('../data/go_import.go', 'r')
-#pattern = r'import[\s\r\n]*\([_\."\w\r\n]*([a-z_/]*)[_\."\w\r\n]*\)'
-pattern = r'import[\s\r\n]*\((?:(?:[^\"\)]|[\s\r\n])*\"([^\"]*)\"[^\"]*)*\)'
-#pattern = r'multiline(?:\r|\n|\r\n|\n\r)*( potato )(?:\r|\n|\r\n|\n\r)*multiline'
-pattern = r'multiline(?:.)*(_potato_)(?:.)*multiline'
-go_import_regex = re.compile(pattern, re.MULTILINE & re.DOTALL)
-# go_import_regex = re.compile(pattern, re.MULTILINE)
-# go_import_regex = re.compile(pattern, re.DOTALL)
+def get_dependencies(path):
+    depends = list()
 
-print("---------- READING FULL SOURCE ---------------")
-full_source = go_source.read()
-match = go_import_regex.search(full_source)
-if match:
-    print(match.groups())
+    # capture single-line imports
+    full_source = ""
+    go_source = open('../data/go_import.go', 'r')
+    for go_line in go_source:
+        # print(go_line)
+        go_line = re.sub("//.*", "", go_line)
+        # print(go_line)
+        deps = re.findall('import[^\"]*\"([^\"]*)\"', go_line)
+        depends.extend(deps)
+        full_source += go_line
 
-print("---------- FULL SOURCE FINDALL ----------------")
-# pattern = r'multiline(?:.)*(_potato_)(?:.)*multiline'
-# print(re.findall(pattern, full_source, re.MULTILINE))
-# print(re.findall(pattern, full_source, re.DOTALL))
-# print(re.findall(pattern, full_source, re.MULTILINE & re.DOTALL))
+    # print(full_source)
+    ptrn = "import[^\(]*\(([^\)]*)\)"
+    # print(re.findall(ptrn, full_source, re.DOTALL))
+    lines = re.findall(ptrn, full_source, re.DOTALL)
+    for line in lines:
+        deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
+        depends.extend(deps)
 
-depends = list()
+    return list(set(depends))
 
-# capture single-line imports
-full_source = ""
-go_source = open('../data/go_import.go', 'r')
-for go_line in go_source:
-    print(go_line)
-    go_line = re.sub("//.*", "", go_line)
-    print(go_line)
-    deps = re.findall('import[^\"]*\"([^\"]*)\"', go_line)
-    depends.extend(deps)
-    full_source += go_line
+print()
+print("Target Raw Data Form")
+owner = "apache"
+repo_name = "ant"
+path = '../data/go_import.go'
+dependencies = get_dependencies(path)
+print('owner\trepo_name\tfile_path\tdependency')
+for dep in dependencies:
+    print(f'{owner}\t{repo_name}\t{path}\t{dep}')
 
-print(full_source)
-# capture multi-line imports
-# go_source = open('../data/go_import.go', 'r')
-# full_source = go_source.read()
-ptrn = "import[^\(]*\(([^\)]*)\)"
-print(re.findall(ptrn, full_source, re.DOTALL))
-lines = re.findall(ptrn, full_source, re.DOTALL)
-for line in lines:
-    deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
-    depends.extend(deps)
+file_dependencies = dict()
+file_dependencies[path] = dependencies
 
-print(list(set(depends)))
+repo_dict = dict()
+repo_dict['owner'] = 'apache'
+repo_dict['repo_name'] = 'ant'
+repo_dict['file_dependencies'] = {path: dependencies}
 
-print("--------- 2 match dotall ---------------")
-mystring = """import ("foo")
-import (
-"bar"
-)
-import ( "zoom" // annoying comment about "dave"
-"schwartz"
-)
-"""
-ptrn = "import \(([^\)]*)\)"
-print(re.findall(ptrn, mystring, re.DOTALL))
-lines = re.findall(ptrn, mystring, re.DOTALL)
-depends = list()
-for line in lines:
-    deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
-    depends.extend(deps)
-print(depends)
+print()
+print("Target JSON Data Form")
+print(json.dumps(repo_dict, indent=2))
+print()
+
+
+def other_old():
+    go_source = open(path, 'r')
+    #pattern = r'import[\s\r\n]*\([_\."\w\r\n]*([a-z_/]*)[_\."\w\r\n]*\)'
+    pattern = r'import[\s\r\n]*\((?:(?:[^\"\)]|[\s\r\n])*\"([^\"]*)\"[^\"]*)*\)'
+    #pattern = r'multiline(?:\r|\n|\r\n|\n\r)*( potato )(?:\r|\n|\r\n|\n\r)*multiline'
+    pattern = r'multiline(?:.)*(_potato_)(?:.)*multiline'
+    go_import_regex = re.compile(pattern, re.MULTILINE & re.DOTALL)
+    # go_import_regex = re.compile(pattern, re.MULTILINE)
+    # go_import_regex = re.compile(pattern, re.DOTALL)
+
+    print("---------- READING FULL SOURCE ---------------")
+    full_source = go_source.read()
+    match = go_import_regex.search(full_source)
+    if match:
+        print(match.groups())
+
+    print("--------- 2 match dotall ---------------")
+    mystring = """import ("foo")
+    import (
+    "bar"
+    )
+    import ( "zoom" // annoying comment about "dave"
+    "schwartz"
+    )
+    """
+    ptrn = "import \(([^\)]*)\)"
+    print(re.findall(ptrn, mystring, re.DOTALL))
+    lines = re.findall(ptrn, mystring, re.DOTALL)
+    depends = list()
+    for line in lines:
+        deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
+        depends.extend(deps)
+    print(depends)
 
 
 def borken():
