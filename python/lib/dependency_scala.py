d7eea79542eb2b2696dd006bb21daddce7a52445
DeadmanBob <github@traxel.com>
2022-10-17 12:08:58 -0700

    scala dependency analyer and some leftovers

---
+++ python/lib/dependency_scala.py
@@ -0,0 +1,21 @@
+import re
+
+class ScalaDependencyAnalyzer:
+    def language(self):
+        return "Scala"
+
+    def matches(self, path):
+        return path.endswith('.scala')
+
+    def get_dependencies(self, path):
+        dependencies = list()
+
+        # full_source = ""
+        source = open(path, 'r')
+        for line in source:
+            line = re.sub("//.*", "", line)
+            deps = re.findall('import[\s]+(?:static[\s]+)?([^ {]*)', line)
+            dependencies.extend(deps)
+            # full_source += line
+        
+        return dependencies
---
+++ sandbox/python/bob/regex/scala_import.py
@@ -0,0 +1,44 @@
+import re
+import json
+import sys
+
+sys.path.append("../../../../python/lib")
+from dependency_scala import ScalaDependencyAnalyzer
+
+repos = list()
+owner = "apache"
+repo_name = "ant"
+paths = [
+    '../data/ScalaImport.scala',
+]
+repo_dict = dict()
+repo_dict['owner'] = owner
+repo_dict['repo_name'] = repo_name
+repo_dict['dependencies'] = dict()
+repos.append(repo_dict)
+
+analyzer = ScalaDependencyAnalyzer()
+for path in paths:
+    if (analyzer.matches(path)):
+        dependencies = analyzer.get_dependencies(path)
+        if analyzer.language() not in repo_dict['dependencies']:
+            repo_dict['dependencies'][analyzer.language()] = list()
+        dep_dict = dict()
+        dep_dict['file_path'] = path
+        dep_dict['dependencies'] = dependencies
+        repo_dict['dependencies'][analyzer.language()].append(dep_dict)
+
+print()
+print("Target JSON Data Form")
+print(json.dumps(repos, indent=2))
+
+print()
+print("Target Raw Data Form")
+print('owner\trepo_name\tlanguage\tfile_path\tdependency')
+for path in paths:
+    if analyzer.matches(path):
+        for dep in analyzer.get_dependencies(path):
+            print(f'{owner}\t{repo_name}\t{analyzer.language()}\t{path}\t{dep}')
+
+print()
+
