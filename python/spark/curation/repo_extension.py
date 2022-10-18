4f12eca49dac5d6ac33add33df148792eefa4800
DeadmanBob <github@traxel.com>
2022-10-18 05:10:49 -0700

    fix the column names in spark/curation/repo_extension.py

--- python/spark/curation/repo_extension.py
+++ python/spark/curation/repo_extension.py
@@ -44,47 +44,47 @@ select js.owner, js.repo,
   js.total_inserts as js_inserts, js.total_deletes as js_deletes,
   js.num_files as js_files, js.total_commits as js_commits,
 -- 7 	.py 	7744299 	2204378266 	30277 	matt 	
-  py.total_inserts as js_inserts, py.total_deletes as js_deletes,
-  py.num_files as js_files, py.total_commits as js_commits,
+  py.total_inserts as py_inserts, py.total_deletes as py_deletes,
+  py.num_files as py_files, py.total_commits as py_commits,
 -- 9 	.c 	7107148 	3344883640 	16678 	matt 	
-  c.total_inserts as js_inserts, c.total_deletes as js_deletes,
-  c.num_files as js_files, c.total_commits as js_commits,
+  c.total_inserts as c_inserts, c.total_deletes as c_deletes,
+  c.num_files as c_files, c.total_commits as c_commits,
 -- 10 	.java 	14416580 	2502998391 	9809 	bob 	done
-  java.total_inserts as js_inserts, java.total_deletes as js_deletes,
-  java.num_files as js_files, java.total_commits as js_commits,
+  java.total_inserts as java_inserts, java.total_deletes as java_deletes,
+  java.num_files as java_files, java.total_commits as java_commits,
 -- 13 	.go 	11739795 	4664082430 	5782 	bob 	done
-  go.total_inserts as js_inserts, go.total_deletes as js_deletes,
-  go.num_files as js_files, go.total_commits as js_commits,
+  go.total_inserts as go_inserts, go.total_deletes as go_deletes,
+  go.num_files as go_files, go.total_commits as go_commits,
 -- 16 	.ts 	5255447 	1137896274 	11218 	matt 	
-  ts.total_inserts as js_inserts, ts.total_deletes as js_deletes,
-  ts.num_files as js_files, ts.total_commits as js_commits,
+  ts.total_inserts as ts_inserts, ts.total_deletes as ts_deletes,
+  ts.num_files as ts_files, ts.total_commits as ts_commits,
 -- 17 	.cpp 	4027283 	1589483149 	11263 	matt 	
-  cpp.total_inserts as js_inserts, cpp.total_deletes as js_deletes,
-  cpp.num_files as js_files, cpp.total_commits as js_commits,
+  cpp.total_inserts as cpp_inserts, cpp.total_deletes as cpp_deletes,
+  cpp.num_files as cpp_files, cpp.total_commits as cpp_commits,
 -- 18 	.php 	6557862 	1316149573 	7282 	bob 	weird
-  php.total_inserts as js_inserts, php.total_deletes as js_deletes,
-  php.num_files as js_files, php.total_commits as js_commits,
+  php.total_inserts as php_inserts, php.total_deletes as php_deletes,
+  php.num_files as php_files, php.total_commits as php_commits,
 -- 22 	.rb 	3048170 	348731047 	9426 	bob 	weird
-  rb.total_inserts as js_inserts, rb.total_deletes as js_deletes,
-  rb.num_files as js_files, rb.total_commits as js_commits,
+  rb.total_inserts as rb_inserts, rb.total_deletes as rb_deletes,
+  rb.num_files as rb_files, rb.total_commits as rb_commits,
 -- 24 	.cs 	3475557 	623920043 	3972 	matt 	
-  cs.total_inserts as js_inserts, cs.total_deletes as js_deletes,
-  cs.num_files as js_files, cs.total_commits as js_commits,
+  cs.total_inserts as cs_inserts, cs.total_deletes as cs_deletes,
+  cs.num_files as cs_files, cs.total_commits as cs_commits,
 -- 25 	.cc 	1352095 	619479180 	6673 	matt 	
-  cc.total_inserts as js_inserts, cc.total_deletes as js_deletes,
-  cc.num_files as js_files, cc.total_commits as js_commits,
+  cc.total_inserts as cc_inserts, cc.total_deletes as cc_deletes,
+  cc.num_files as cc_files, cc.total_commits as cc_commits,
 -- 35 	.rs 	964655 	295521616 	2991 	bob 	hard
-  rs.total_inserts as js_inserts, rs.total_deletes as js_deletes,
-  rs.num_files as js_files, rs.total_commits as js_commits,
+  rs.total_inserts as rs_inserts, rs.total_deletes as rs_deletes,
+  rs.num_files as rs_files, rs.total_commits as rs_commits,
 -- 38 	.tsx 	839036 	111013387 	3969 	matt 	
-  tsx.total_inserts as js_inserts, tsx.total_deletes as js_deletes,
-  tsx.num_files as js_files, tsx.total_commits as js_commits,
+  tsx.total_inserts as tsx_inserts, tsx.total_deletes as tsx_deletes,
+  tsx.num_files as tsx_files, tsx.total_commits as tsx_commits,
 -- 54 	.scala 	926666 	123227766 	1414 	bob 	should be doable
-  scala.total_inserts as js_inserts, scala.total_deletes as js_deletes,
-  scala.num_files as js_files, scala.total_commits as js_commits,
+  scala.total_inserts as scala_inserts, scala.total_deletes as scala_deletes,
+  scala.num_files as scala_files, scala.total_commits as scala_commits,
 -- 55 	.jsx 	345476 	51432270 	3575 	matt 	
-  jsx.total_inserts as js_inserts, jsx.total_deletes as js_deletes,
-  jsx.num_files as js_files, jsx.total_commits as js_commits
+  jsx.total_inserts as jsx_inserts, jsx.total_deletes as jsx_deletes,
+  jsx.num_files as jsx_files, jsx.total_commits as jsx_commits
 -- 1 	.js 	26752659 	6782193447 	35514 	matt 	
 from repo_ext js
 -- 7 	.py 	7744299 	2204378266 	30277 	matt 	
