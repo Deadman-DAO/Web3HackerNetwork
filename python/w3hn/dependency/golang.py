7b35392bebbad8d6fdaea9c92a3c4ad5fa717005
DeadmanBob <github@traxel.com>
2022-10-20 17:33:04 -0700

    decapitate golang.py for the moment

--- python/w3hn/dependency/golang.py
+++ python/w3hn/dependency/golang.py
@@ -9,23 +9,24 @@ class GoDependencyAnalyzer:
         return path.endswith('.go')
         
     def get_dependencies(self, path):
-        depends = list()
+        return None
+        # depends = list()
 
-        # capture single-line imports
-        # side-effect: remove '//'-style comments
-        full_source = ""
-        with open(path, 'r') as go_source:
-            for go_line in go_source:
-                go_line = re.sub("//.*", "", go_line)
-                deps = re.findall('import[^\"]*\"([^\"]*)\"', go_line)
-                depends.extend(deps)
-                full_source += go_line
+        # # capture single-line imports
+        # # side-effect: remove '//'-style comments
+        # full_source = ""
+        # with open(path, 'r') as go_source:
+        #     for go_line in go_source:
+        #         go_line = re.sub("//.*", "", go_line)
+        #         deps = re.findall('import[^\"]*\"([^\"]*)\"', go_line)
+        #         depends.extend(deps)
+        #         full_source += go_line
 
-        # capture multi-line imports
-        ptrn = "import[^\(]*\(([^\)]*)\)"
-        lines = re.findall(ptrn, full_source, re.DOTALL)
-        for line in lines:
-            deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
-            depends.extend(deps)
+        # # capture multi-line imports
+        # ptrn = "import[^\(]*\(([^\)]*)\)"
+        # lines = re.findall(ptrn, full_source, re.DOTALL)
+        # for line in lines:
+        #     deps = re.findall('[^\"]*\"([^\"]*)\"[^\"]*', line, re.DOTALL)
+        #     depends.extend(deps)
 
-        return list(set(depends))
+        # return list(set(depends))
