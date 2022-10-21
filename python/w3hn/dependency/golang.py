a15293a9889882f8a78b29b3dca36b0624e43d35
enigmatt <!@#$^%&*()nada>
2022-10-21 11:20:01 -0700

    Still chasing bugs - adding psuedo-test functions to golang.py and python.py

--- python/w3hn/dependency/golang.py
+++ python/w3hn/dependency/golang.py
@@ -1,4 +1,8 @@
 import re
+import os
+import sys
+relative_lib = "../.."
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), relative_lib))
 
 
 class GoDependencyAnalyzer:
@@ -29,3 +33,8 @@ class GoDependencyAnalyzer:
             depends.extend(deps)
 
         return list(set(depends))
+
+
+if __name__ == '__main__':
+    file = sys.argv[1] if len(sys.argv) > 1 else '../../../data/samples/source/go/ir.go'
+    print(GoDependencyAnalyzer().get_dependencies(file))
