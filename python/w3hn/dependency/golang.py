f2744611271f241f2d6083b8c8df847b4d48fea4
enigmatt <!@#$^%&*()nada>
2022-10-21 11:30:10 -0700

    Rolling back debug "main()" approach.

--- python/w3hn/dependency/golang.py
+++ python/w3hn/dependency/golang.py
@@ -1,8 +1,4 @@
 import re
-import os
-import sys
-relative_lib = "../.."
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), relative_lib))
 
 
 class GoDependencyAnalyzer:
@@ -33,8 +29,3 @@ class GoDependencyAnalyzer:
             depends.extend(deps)
 
         return list(set(depends))
-
-
-if __name__ == '__main__':
-    file = sys.argv[1] if len(sys.argv) > 1 else '../../../data/samples/source/go/ir.go'
-    print(GoDependencyAnalyzer().get_dependencies(file))
