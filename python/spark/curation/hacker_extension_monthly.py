3090bbd76e334e305ba5995d6dd6fd2404f8d35a
DeadmanBob <github@traxel.com>
2022-11-02 10:17:35 -0700

    Spark/Logging/Functions upgrade for curation/hacker_extension_monthly

--- python/spark/curation/hacker_extension_monthly.py
+++ python/spark/curation/hacker_extension_monthly.py
@@ -1,30 +1,62 @@
+# ----- BEGIN SPARK JOB BOILERPLATE --------------------------------
+import boto3
+import logging
 import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+from pyspark.sql import SparkSession
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+job_name = sys.argv[0]
+if '--JOB_NAME' in sys.argv:
+    if len(sys.argv) > sys.argv.index('--JOB_NAME') + 1:
+        job_name = sys.argv[sys.argv.index('--JOB_NAME') + 1]
 
-output_path = "s3://deadmandao/web3hackernetwork/data_pipeline/curated/hacker_extension_monthly"
-
+pipeline_path = 'web3hackernetwork/data_pipeline'
 sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+spark = SparkSession.builder.config("k1", "v1").getOrCreate()
 
-## Dunno what this is for, the AWS example doesn't have it:
-## https://github.com/aws-samples/aws-glue-samples/blob/master/examples/join_and_relationalize.py
-# job = Job(glueContext)
-# job.init(args['JOB_NAME'], args)
-# job.commit()
+def get_logger(name):
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter(fmt))
+    logging.getLogger().setLevel(logging.WARNING)
+    logging.getLogger().addHandler(handler)
+    log = logging.getLogger(name=name)
+    log.setLevel(logging.INFO)
+    return log
 
-glue_hacker_file = glueContext.create_dynamic_frame.from_catalog(database='w3hn', table_name='raw_file_hacker')
-hacker_data_frame = glue_hacker_file.toDF() #Convert to Spark data frame
-hacker_data_frame.registerTempTable("temp_file_hacker") #Register in-memory table accessible from sparc
+log = get_logger(f'{job_name}')
 
-job_sql = """
+def delete_recursive(bucket, prefix):
+    log.info(f'recursive delete: s3:// {bucket} / {prefix}')
+    boto3_s3 = boto3.session.Session().client('s3')
+    response = boto3_s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for object in response['Contents']:
+        key = object['Key']
+        log.info(f'deleting s3://{bucket}/{key}')
+        boto3_s3.delete_object(Bucket=bucket, Key=key)
+
+def register_table(spark, bucket, tier, name):
+    path = f's3://{bucket}/{pipeline_path}/{tier}/{name}'
+    table = f'{tier}_{name}'
+    log.info(f'registering {path} as table {table}')
+    df = spark.read.parquet(path)
+    df.registerTempTable(table)
+
+def insert_or_update(df, bucket, tier, name):
+    key = f'{pipeline_path}/{tier}/{name}'
+    try:
+        delete_recursive(bucket, key)
+    except Exception as e:
+        log.error(str(e))
+    url = f's3://{bucket}/{key}'
+    log.info(f'writing datset to {url}')
+    df.write.parquet(url)
+# ----- END SPARK JOB BOILERPLATE ----------------------------------
+
+register_table(spark, 'deadmandao', 'raw', 'file_hacker')
+
+out_sql = """
 select author,
   substr(file_path, 1 + length(file_path) - position('.' in reverse(file_path))) as extension,
   date_trunc('MM', commit_date) as year_month,
@@ -32,11 +64,13 @@ select author,
   sum(total_deletes) as total_deletes,
   count(distinct(commit_date)) as num_commits,
   count(distinct(file_path)) as num_files
-from temp_file_hacker
+from raw_file_hacker
 where binary = 0
 and substr(file_path, 1 + length(file_path) - position('.' in reverse(file_path))) in ('.js', '.py', '.c', '.java', '.go', '.ts', '.cpp', '.php', '.rb', '.cs', '.cc', '.rs', '.tsx', '.scala', '.jsx')
 group by author, substr(file_path, 1 + length(file_path) - position('.' in reverse(file_path))), date_trunc('MM', commit_date)
 order by author, extension, year_month
 """
-out_df = spark.sql(job_sql)
-out_df.coalesce(1).write.parquet(output_path)
+
+log.info(f'executing sql:\n{out_sql}')
+out_df = spark.sql(out_sql).coalesce(1)
+insert_or_update(out_df, 'deadmandao', 'curated', 'hacker_extension_monthly')
