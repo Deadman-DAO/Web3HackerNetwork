1c69732cfb89e1cdb4d5ca573ee4650b0dad0bad
DeadmanBob <github@traxel.com>
2022-11-15 13:46:59 -0700

    working on ASV TF-IDF

--- python/sandbox/bob/bible/asv-ingest.py
+++ python/sandbox/bob/bible/asv-ingest.py
@@ -9,8 +9,60 @@ from w3hn.aws.aws_util import S3Util
 from w3hn.log.log_init import logger
 log = logger(__file__)
 
-COLUMN_NAMES = ['book', 'chapter', 'verse', 'text']
+OT = ['Genesis', 'Exodus', 'Leviticus', 'Numbers',
+      'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel',
+      '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles',
+      '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job',
+      'Psalm', 'Proverbs', 'Ecclesiastes', 'Song of Solomon',
+      'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel',
+      'Daniel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah',
+      'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai',
+      'Zechariah', 'Malachi']
+
+NT = ['Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans',
+      '1 Corinthians', '2 Corinthians', 'Galatians',
+      'Ephesians', 'Philippians', 'Colossians',
+      '1 Thessalonians', '2 Thessalonians', '1 Timothy',
+      '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James',
+      '1 Peter', '2 Peter', '1 John', '2 John', '3 John',
+      'Jude', 'Revelation']
+
+SUBCATEGORIES = {
+    'moses school': ['Genesis', 'Exodus', 'Leviticus',
+                     'Numbers', 'Deuteronomy'],
+    'history': ['Joshua', 'Judges', 'Ruth', '1 Samuel',
+                '2 Samuel', '1 Kings', '2 Kings',
+                '1 Chronicles', '2 Chronicles', 'Ezra',
+                'Nehemiah', 'Esther', 'Job'],
+    'wisdom literature': ['Psalm', 'Proverbs',
+                          'Ecclesiastes', 'Song of Solomon'],
+    'major prophets': ['Isaiah', 'Jeremiah', 'Lamentations',
+                       'Ezekiel', 'Daniel'],
+    'minor prophets': ['Hosea', 'Joel', 'Amos', 'Obadiah',
+                       'Jonah', 'Micah', 'Nahum', 'Habakkuk',
+                       'Zephaniah', 'Haggai', 'Zechariah',
+                       'Malachi'],
+    'gospel': ['Matthew', 'Mark', 'Luke', 'John'],
+    'after ascension': ['Acts'],
+    'letters': [
+        'Romans', '1 Corinthians', '2 Corinthians', 'Galatians',
+        'Ephesians', 'Philippians', 'Colossians',
+        '1 Thessalonians', '2 Thessalonians', '1 Timothy',
+        '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James',
+        '1 Peter', '2 Peter', '1 John', '2 John', '3 John',
+        'Jude'
+    ],
+    'apocalyptic literature': ['Revelation']
+}
+
+COLUMN_NAMES = ['religion', 'tome', 'version', 'category',
+                'subcategory', 'book', 'chapter', 'verse', 'text']
 SCHEMA = pa.schema([
+    pa.field('religion', pa.string()),
+    pa.field('tome', pa.string()),
+    pa.field('version', pa.string()),
+    pa.field('category', pa.string()),
+    pa.field('subcategory', pa.string()),
     pa.field('book', pa.string()),
     pa.field('chapter', pa.int32()),
     pa.field('verse', pa.int32()),
@@ -20,6 +72,11 @@ SCHEMA = pa.schema([
 asv_bzip_path = 'data/samples/bible-american-standard-version-asv.txt.bz2'
 with bz2.open(asv_bzip_path, 'rt') as asv_in:
     lines = asv_in.readlines()
+    religions = []
+    tomes = []
+    versions = []
+    categories = []
+    subcategories = []
     books = []
     chapters = []
     verses = []
@@ -31,14 +88,29 @@ with bz2.open(asv_bzip_path, 'rt') as asv_in:
             text = parts[1]
             parts = re.findall('^(.*) ([0-9]*):([0-9]*)$', book_ch_verse)
             book = parts[0][0]
+            if book in OT:
+                testament = 'old testament'
+            else:
+                testament = 'new testament'
             chapter = parts[0][1]
             verse = parts[0][2]
             # print(f'{book}\t{chapter}\t{verse}\t{text}')
+            religions.append('protestant')
+            tomes.append('bible')
+            versions.append('asv')
+            categories.append(testament)
+            book_subcategory = 'unknown'
+            for subcategory, member_books in SUBCATEGORIES.items():
+                if book in member_books:
+                    # print(f'{book} = {subcategory}: {member_books}')
+                    book_subcategory = subcategory
+            subcategories.append(book_subcategory)
             books.append(book)
             chapters.append(chapter)
             verses.append(verse)
             texts.append(text)
-    data = [books, chapters, verses, texts]
+    data = [religions, tomes, versions, categories, subcategories,
+            books, chapters, verses, texts]
 
 batch = pa.RecordBatch.from_arrays(data, COLUMN_NAMES)
 table = pa.Table.from_batches([batch]).cast(SCHEMA)
@@ -50,10 +122,15 @@ dataset_path = 'web3hackernetwork/data_pipeline/raw/bible_asv'
 s3_util = S3Util(profile='w3hn-admin', bucket_name=bucket)
 s3fs = s3_util.pyarrow_fs()
 bucket_path = f'{bucket}/{dataset_path}'
-log.debug(f'sorting')
-table.sort_by([('book', 'ascending'),
-               ('chapter', 'ascending'),
-               ('verse', 'ascending')])
+# log.debug(f'sorting')
+# table.sort_by([('religion', 'ascending'),
+#                ('tome', 'descending'),
+#                ('version', 'descending'),
+#                ('category', 'descending'),
+#                ('subcategory', 'descending'),
+#                ('book', 'ascending'),
+#                ('chapter', 'ascending'),
+#                ('verse', 'ascending')])
 log.info(f'writing {bucket}/{dataset_path}')
 if s3_util.path_exists(dataset_path):
     log.info(f'deleting {bucket}/{dataset_path}')
