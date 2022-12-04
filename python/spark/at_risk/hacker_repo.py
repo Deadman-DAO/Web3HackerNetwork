cbe5ad0db0b9c19151a5f4faff827f3c48477fcc
DeadmanBob <github@traxel.com>
2022-12-03 20:33:28 -0700

    rough pass at publishing data for use on our website and consuming it from Django

---
+++ python/spark/at_risk/hacker_repo.py
@@ -0,0 +1,78 @@
+# --- Run Stats ---------------------------
+# 2022-11-02: 2 @ G.2X (4 DPU): 
+# -----------------------------------------
+
+# ----- BEGIN SPARK JOB BOILERPLATE --------------------------------
+import boto3
+import logging
+import sys
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+
+job_name = sys.argv[0]
+if '--JOB_NAME' in sys.argv:
+    if len(sys.argv) > sys.argv.index('--JOB_NAME') + 1:
+        job_name = sys.argv[sys.argv.index('--JOB_NAME') + 1]
+
+pipeline_path = 'web3hackernetwork/data_pipeline'
+sc = SparkContext()
+spark = SparkSession.builder.config("k1", "v1").getOrCreate()
+
+def get_logger(name):
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter(fmt))
+    logging.getLogger().setLevel(logging.WARNING)
+    logging.getLogger().addHandler(handler)
+    log = logging.getLogger(name=name)
+    log.setLevel(logging.INFO)
+    return log
+
+log = get_logger(f'{job_name}')
+
+def delete_recursive(bucket, prefix):
+    log.info(f'recursive delete: s3:// {bucket} / {prefix}')
+    boto3_s3 = boto3.session.Session().client('s3')
+    response = boto3_s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for object in response['Contents']:
+        key = object['Key']
+        log.info(f'deleting s3://{bucket}/{key}')
+        boto3_s3.delete_object(Bucket=bucket, Key=key)
+
+def register_table(spark, bucket, tier, name):
+    path = f's3://{bucket}/{pipeline_path}/{tier}/{name}'
+    table = f'{tier}_{name}'
+    log.info(f'registering {path} as table {table}')
+    df = spark.read.parquet(path)
+    df.registerTempTable(table)
+
+def insert_or_update(df, bucket, tier, name):
+    key = f'{pipeline_path}/{tier}/{name}'
+    try:
+        delete_recursive(bucket, key)
+    except Exception as e:
+        log.error(str(e))
+    url = f's3://{bucket}/{key}'
+    log.info(f'writing datset to {url}')
+    df.write.parquet(url)
+# ----- END SPARK JOB BOILERPLATE ----------------------------------
+
+register_table( spark, 'deadmandao', 'curated', 'repo_hacker')
+register_table( spark, 'deadmandao', 'athena', 'targetable_hackers')
+
+out_sql = """
+SELECT rh.owner, rh.repo_name, rh.partition_key, rh.owner_repo,
+       regexp_extract(rh.author, '(.*) <(.*)@(.*)>', 1) AS author_name,
+       regexp_extract(rh.author, '(.*) <(.*)@(.*)>', 3) AS author_email_domain,
+       md5(regexp_extract(rh.author, '(.*) <(.*)>', 2)) AS author_email_hash
+FROM curated_repo_hacker rh
+WHERE EXISTS (
+  SELECT 1 FROM athena_targetable_hackers ah
+    WHERE ah.author = rh.author
+)
+"""
+
+log.info(f'executing sql:\n{out_sql}')
+out_df = spark.sql(out_sql).coalesce(1)
+insert_or_update(out_df, 'deadmandao-at-risk', 'web_data', 'repo_hacker')
