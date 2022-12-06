8f0fea878f1adbbe6e3206f86e30e368b493ca99
enigmatt <git@theenkes.com>
2022-12-06 14:20:29 -0700

    GitHub Spy helps again!

--- python/sandbox/matt/http/SearchByEmail.js
+++ python/sandbox/matt/http/SearchByEmail.js
@@ -22,27 +22,26 @@ function jqLoaded() {
     debug("jQuery Loaded");
     firePageLoaded();
 }
-
+function populate_table(table_id, data_array) {
+    let table = $(table_id);
+    let tableBody = table.find('tbody');
+    tableBody.empty();
+    data_array.forEach(function (row_data) {
+        let row = $('<tr></tr>');
+        row_data.forEach(function (cell_data) {
+            let cell = $('<td></td>');
+            cell.text(cell_data);
+            row.append(cell);
+        });
+        tableBody.append(row);
+    });
+}
 function search_by_email() {
     let email = document.getElementById("email").value;
     let email_hash = hex_md5(email);
     debug('Searching for '+email);
     $.get('/api/searchByEmail/?email_hash='+email_hash, function (rslt) {
-        pastReposTableJQ = $('#beenthere');
-        pastReposTable = pastReposTableJQ[0];
-        while (pastReposTable.rows.length > 1) {
-            pastReposTable.deleteRow(1);
-        }
-        rslt.projects.forEach(function (project) {
-            let row = pastReposTable.appendChild(document.createElement('tr'));
-            let col = row.appendChild(document.createElement('td'));
-            col.appendChild(document.createTextNode(project.owner));
-            col = row.appendChild(document.createElement('td'));
-            col.appendChild(document.createTextNode(project.repo_name));
-        });
-        pastReposTableJQ.removeClass('hidden');
-        pastReposTableJQ.addClass('visible');
-        debug(rslt);
+        populate_table('#beenthere', rslt.projects)
     });
 }
 
