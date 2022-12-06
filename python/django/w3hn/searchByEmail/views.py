7654f89cdf1e3864ab64c5fa12c02fd46eb2542f
DeadmanBob <github@traxel.com>
2022-12-06 13:02:46 -0700

    all three JSON outputs working

--- python/django/w3hn/searchByEmail/views.py
+++ python/django/w3hn/searchByEmail/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 import boto3
 import configparser
 import duckdb
+import json
 import os
 import pyarrow.fs as pafs
 import pyarrow.parquet as papq
 
 # pip install boto3 duckdb pyarrow pandas # logging
+
+pipeline_path = 'web3hackernetwork/data_pipeline'
 
 def read_parquet_table(profile, bucket, path):
     cred_path = os.path.expanduser("~")+"/.aws/credentials"
@@ -18,28 +21,79 @@ def read_parquet_table(profile, bucket, path):
     fs = pafs.S3FileSystem(access_key=key_id, secret_key=secret)
     return papq.read_table(f'{bucket}/{path}', filesystem=fs)
 
-def index(request):
-    email_hash = request.GET.get("email_hash")
-    email_hash_type = type(email_hash)
-    # pipeline_path = 'web3hackernetwork/data_pipeline'
-    # dataset_path = f'{pipeline_path}/athena/targetable_hackers/20221129_052341_00041_9bdqq_998d7f1d-e656-47d1-bab5-7ea0acafeaa4'
-    # targetable = read_parquet_table('w3hn-admin', 'deadmandao', dataset_path)
-    # conn = duckdb.connect()
-    # sql = "SELECT author FROM targetable WHERE author LIKE 'Dav%'"
-    # df = conn.query(sql).to_df()
-    # authors = [a.replace('<', '&lt;') for a in list(df['author'])]
-    # authors = [a.replace('>', '&gt;') for a in authors]
-    # authors = [f'<li>{a}</li>' for a in authors]
-    # authors = '\n'.join(authors)
-    page = f"""
-        <html>
-          <head><title>I'll be your commander.</title></head>
-          <body>
-            <h1>Hello, World!</h1>
-            <ul>
-              {email_hash_type}: '{email_hash}'
-            </ul>
-          </body>
-        </html>
+def get_aliases(requested_email_hash):
+    dataset_path = f'{pipeline_path}/web_data/hacker_alias/'
+    repo_hacker = read_parquet_table('w3hn-at-risk', 'deadmandao-at-risk', dataset_path)
+    conn = duckdb.connect()
+    sql = f"""
+      SELECT distinct alias_name, alias_email_domain, alias_email_hash
+        FROM repo_hacker
+        WHERE author_email_hash = '{requested_email_hash}'
     """
-    return HttpResponse(page)
+    df = conn.query(sql).to_df()
+    names = list(df['alias_name'])
+    domains = list(df['alias_email_domain'])
+    hashes = list(df['alias_email_hash'])
+    zipped = zip(names, domains, hashes)
+    aliases = [{'name':n, 'domain':d, 'hash':h} for (n, d, h) in zipped]
+    return aliases
+
+def get_recommendations(requested_email_hash):
+    dataset_path = f'{pipeline_path}/web_data/hacker_suggestion/'
+    hacker_suggestion = read_parquet_table('w3hn-at-risk',
+                                           'deadmandao-at-risk',
+                                           dataset_path)
+    conn = duckdb.connect()
+    sql = f"""
+      SELECT distinct owner, repo_name, license, num_stars, num_forks
+        FROM hacker_suggestion
+        WHERE author_email_hash = '{requested_email_hash}'
+        ORDER BY score DESC
+    """
+    df = conn.query(sql).to_df()
+    owners = list(df['owner'])
+    repos = list(df['repo_name'])
+    licenses = list(df['license'])
+    num_starss = list(df['num_stars'])
+    num_forkss = list(df['num_forks'])
+    zipped = zip(owners, repos, licenses, num_starss, num_forkss)
+    recommendations= [{
+        'owner':o,
+        'repo_name':r,
+        'license':l,
+        'num_stars':s,
+        'num_forks':f,
+    } for (o,r,l,s,f) in zipped]
+    return recommendations
+
+def get_projects(requested_email_hash):
+    dataset_path = f'{pipeline_path}/web_data/repo_hacker/'
+    repo_hacker = read_parquet_table('w3hn-at-risk', 'deadmandao-at-risk', dataset_path)
+    conn = duckdb.connect()
+    sql = f"""
+      SELECT distinct owner, repo_name
+        FROM repo_hacker
+        WHERE author_email_hash = '{requested_email_hash}'
+    """
+    df = conn.query(sql).to_df()
+    owners = list(df['owner'])
+    repos = list(df['repo_name'])
+    zipped = zip(owners, repos)
+    projects= [{
+        'owner':o,
+        'repo_name':r,
+    } for (o,r) in zipped]
+    return projects
+
+def index(request):
+    requested_email_hash = request.GET.get("email_hash")
+    aliases = get_aliases(requested_email_hash)
+    projects = get_projects(requested_email_hash)
+    recommendations = get_recommendations(requested_email_hash)
+    outer_dict = {
+        'aliases': aliases,
+        'projects': projects,
+        'recommendations': recommendations,
+    }
+    jason = json.dumps(outer_dict, indent=2)
+    return HttpResponse(jason, content_type='application/json')
