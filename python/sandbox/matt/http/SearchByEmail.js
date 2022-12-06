e912b91452c2e6b1af86232d89cf112c3e39225e
enigmatt <git@theenkes.com>
2022-12-06 15:14:51 -0700

    Trying to add a link to the actual GitHub site.

--- python/sandbox/matt/http/SearchByEmail.js
+++ python/sandbox/matt/http/SearchByEmail.js
@@ -27,6 +27,7 @@ function populate_table(table_id, data_array, columns) {
     let tableBody = table.find('tbody');
     tableBody.empty();
     data_array.forEach(function (row_data) {
+        let link = $('<a></a>').attr('href', 'https://github.com/'+row_data.owner+'/'+row_data.repo_name);
         let row = $('<tr></tr>');
         row.addClass('row');
         columns.forEach(function (cell_name) {
@@ -35,7 +36,8 @@ function populate_table(table_id, data_array, columns) {
             cell.text(cell_data);
             row.append(cell);
         });
-        tableBody.append(row);
+        link.append(row);
+        tableBody.append(link);
     });
     table.removeClass('invisible');
     table.addClass('visible')
