8fa9a1ad44d1ef3bed2fa04d4c7da7fc0431f6d1
DeadmanBob <github@traxel.com>
2023-01-02 12:11:24 -0700

    Add the pk prefix to the partition key. Again. For the first time.

--- python/w3hn/hadoop/parquet_util.py
+++ python/w3hn/hadoop/parquet_util.py
@@ -5,4 +5,7 @@ def repo_partition_key(owner, repo_name):
     # print(f'Partition Name: {partition_name}')
     key_hash = hashlib.md5(partition_name.encode('utf-8')).hexdigest()
     synthetic_key = key_hash[slice(0, 2)]
-    return synthetic_key
+    return f'pk{synthetic_key}'
+
+def owner_repo(owner, repo_name):
+    return f'{owner}/{repo_name}'
