26a550db31ea65dc9ca5337202054dca3a477345
enigmatt <git@theenkes.com>
2023-01-03 18:47:42 -0700

    Revert "Oops.  Fixing one bug caused another one.  The numstat process was counting on the "repo_machine_name" remaining populated too. (Bad coffy & pastry)"
    This reverts commit 228799acd0e9ab8abf62b05eaecc47542d15e7b1.

--- sql/mattbox/ReleaseRepoFromCloning.sql
+++ sql/mattbox/ReleaseRepoFromCloning.sql
@@ -20,7 +20,7 @@ BEGIN
 		last_analysis_date = case when _success then null else dt end ,
 		numstat_dir = case when _success then null else _repo_dir end ,
 		numstat_machine_name = case when _success then null else _machine_name end ,
-        repo_machine_name = _machine_name ,
+        repo_machine_name = case when _success then null else _machine_name end ,
 		failed_date = case when _success then null else dt end 
 	 where id = _repo_id;
 	update repo_reserve set tstamp = dt where repo_id = _repo_id;
