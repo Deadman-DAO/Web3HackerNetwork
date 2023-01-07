90319ed9eef0e22a4c25231e0392282fa38e4879
enigmatt <git@theenkes.com>
2023-01-07 14:36:53 -0700

    Tweeek!  Still just getting a basic list of the S3 bucket contents, then adding how many have an identical signature of file names.

--- python/lib/S3_Cleanup.py
+++ python/lib/S3_Cleanup.py
@@ -1,90 +1,93 @@
 import boto3
 import json
 import os
-# Create an S3 client
-s3 = boto3.client('s3')
+from lib.db_dependent_class import DBDependent
 
-# Set the name of the bucket that you want to scan
-bucket_name = 'numstat-bucket'
+class S3Cleanup(DBDependent):
+    def __init__(self):
+        DBDependent.__init__(self)
+        self.s3 = boto3.resource('s3')
+        self.bucket_name = 'numstat-bucket'
+        self.bucket = self.s3.Bucket(self.bucket_name)
+        self.prefix = 'repo_v01/'
 
-# Set the prefix for the objects that you want to list.
-# This will list only objects that have this prefix in their key.
-prefix = 'repo_v01/'
+        self.max_keys = 1000
 
-# Set the maximum number of objects that you want to receive in each response
-max_keys = 10000
+        self.continuation_token = None
+        self.owner_map = {}
+        self.running = True
+        self.item_count = 0
+        self.repo_count = 0
 
-# Set the continuation token to an empty string to start listing objects
-continuation_token = None
-owner_map = {}
-running = True
-item_count = 0
-repo_count = 0
-
-if os.path.exists('./repo_bucket_contents.json'):
-    with open('./repo_bucket_contents.json', 'r') as f:
-        owner_map = json.loads(f.read())
-else:
-    while running:
-        # Call the list_objects_v2 method to list the objects in the bucket
-        if continuation_token is None:
-            response = s3.list_objects_v2(
-                Bucket=bucket_name,
-                Prefix=prefix,
-                MaxKeys=max_keys
-            )
+    def load_info(self):
+        if os.path.exists('./repo_bucket_contents.json'):
+            with open('./repo_bucket_contents.json', 'r') as f:
+                self.owner_map = json.loads(f.read())
         else:
-            response = s3.list_objects_v2(
-                Bucket=bucket_name,
-                Prefix=prefix,
-                MaxKeys=max_keys,
-                ContinuationToken=continuation_token
-            )
+            while self.running:
+                # Call the list_objects_v2 method to list the objects in the bucket
+                if self.continuation_token is None:
+                    response = self.s3.list_objects_v2(
+                        Bucket=self.bucket_name,
+                        Prefix=self.prefix,
+                        MaxKeys=self.max_keys
+                    )
+                else:
+                    response = self.s3.list_objects_v2(
+                        Bucket=self.bucket_name,
+                        Prefix=self.prefix,
+                        MaxKeys=self.max_keys,
+                        ContinuationToken=self.continuation_token
+                    )
 
 
-        # Get the list of objects from the response
-        if 'Contents' in response:
-            objects = response['Contents']
-            for obj in objects:
-                # Print the object key
-                elems = obj['Key'].split('/')
-                size = obj['Size']
-                if len(elems) == 4:
-                    owner = elems[1]
-                    repo = elems[2]
-                    owner_repo = owner + '/' + repo
-                    file_name = elems[3]
-                    item_count += 1
-                    if owner_repo in owner_map:
-                        files = owner_map[owner_repo]
-                        files.append(file_name)
-                    else:
-                        repo_count += 1
-                        owner_map[owner_repo] = [file_name]
+                # Get the list of objects from the response
+                if 'Contents' in response:
+                    objects = response['Contents']
+                    for obj in objects:
+                        # Print the object key
+                        elems = obj['Key'].split('/')
+                        size = obj['Size']
+                        if len(elems) == 4:
+                            owner = elems[1]
+                            repo = elems[2]
+                            owner_repo = owner + '/' + repo
+                            file_name = elems[3]
+                            self.item_count += 1
+                            if owner_repo in self.owner_map:
+                                files = self.owner_map[owner_repo]
+                                files.append(file_name)
+                            else:
+                                self.repo_count += 1
+                                self.owner_map[owner_repo] = [file_name]
 
 
-        # Get the continuation token from the response, if there is one
-        continuation_token = response.get('NextContinuationToken', None)
+                # Get the continuation token from the response, if there is one
+                continuation_token = response.get('NextContinuationToken', None)
 
-        # If there is no continuation token, we have reached the end of the list of objects
-        if continuation_token is None:
-            running = False
-        else:
-            print(f'Continuation token: {continuation_token} ({item_count} items, {repo_count} repos)')
+                # If there is no continuation token, we have reached the end of the list of objects
+                if continuation_token is None:
+                    running = False
+                else:
+                    print(f'Continuation token: {continuation_token} ({self.item_count} items, {self.repo_count} repos)')
 
-    with open('repo_bucket_contents.json', 'w') as f:
-        f.write(json.dumps(owner_map, indent=4))
+            with open('repo_bucket_contents.json', 'w') as f:
+                f.write(json.dumps(self.owner_map, indent=4))
 
-count_map = {}
-unique_map = {}
+    def run(self):
+        self.load_info()
+        print(f'Loaded {self.item_count} items, {self.repo_count} repos')
 
-for owner_repo, file_list in owner_map.items():
-    dicky = str(file_list)
-    if dicky in unique_map:
-        count_map[dicky] += 1
-    else:
-        count_map[dicky] = 1
-        unique_map[dicky] = file_list
+        count_map = {}
+        unique_map = {}
 
-for key, value in count_map.items():
-    print(f'{key} occurred {value} times')
+        for owner_repo, file_list in self.owner_map.items():
+            dicky = str(file_list)
+            if dicky in unique_map:
+                count_map[dicky] += 1
+            else:
+                count_map[dicky] = 1
+                unique_map[dicky] = file_list
+
+        for key, value in count_map.items():
+            print(f'{key} occurred {value} times')
